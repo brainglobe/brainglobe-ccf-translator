608e3c3899df63c42071f76734a143e394c10b8b
polarbean <harry.carey95@gmail.com>
2024-11-18 16:51:01 +0100

    following brainglobe cookie-cutter standardisation

--- brainglobe_ccf_translator/__init__.py
+++ brainglobe_ccf_translator/__init__.py
@@ -1,3 +1,11 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("brainglobe-ccf-translator")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
 from . import *
 from .PointSet import PointSet
 from .VolumeSeries import VolumeSeries
